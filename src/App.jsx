6f4edb04e6fbd549a3afab98ffe406a56b7e679e
M N Shakib <mnshakib79@gmail.com>
2023-09-18 14:39:22 +0530

    feat: app initialize

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=" text-center text-xl">
+        let's start
+      </div>
+    </>
+  )
+}
+
+export default App
